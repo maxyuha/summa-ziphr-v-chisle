807abcb27c8e4364ae28c41f8347ad8e1628f860
Maksim <maxyuha1998@gmail.com>
2022-07-24 21:43:56 +0300

    make task 27

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsolleApp
+{
+     class Program
+     {
+        static void Main(string[] args)
+        {
+            void Zadacha27()
+            {//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+            Console.WriteLine("введите число");
+            int number=Convert.ToInt32(Console.ReadLine());
+             int sum=0;
+             Console.Write("в числе " + number);
+             while(number>0)
+             {
+                sum+=number%10;
+                number/=10;
+
+             } 
+             Console.WriteLine( " сумма цифр равна " + sum);  
+            }
+            Zadacha27();
+        }
+        
+     }
+    
+}
